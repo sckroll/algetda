6d68c2e1c1d5912226de14526e69d6cec6c269b5
sckroll <kimsc0714@gmail.com>
2021-11-24 16:55:57 +0900

    feat(style): 헤더 메뉴 레이아웃 구성

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <header class="header-bar">
+      <div class="left">
+        <div class="drawer-icon"></div>
+        <div class="logo">Algetda</div>
+        <div class="curr-algorithm"></div>
+      </div>
+      <div class="right">
+        <div class="gear-icon"></div>
+        <div class="home-icon"></div>
+        <div class="github-icon"></div>
+      </div>
+    </header>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+header.header-bar {
+  height: 64px;
+  padding: 16px;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: $color-grey-4;
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .left,
+  .right {
+    display: flex;
+    align-items: center;
+    gap: 16px;
   }
+}
+.logo {
+  font-size: 1.5em;
+}
+.drawer-icon,
+.gear-icon,
+.home-icon,
+.github-icon {
+  width: 32px;
+  height: 32px;
+  background-color: $color-grey-3;
 }
 </style>
