2691f3fbe859e18376ac94b0e3ecce64aeaa08fa
sckroll <kimsc0714@gmail.com>
2021-11-24 16:55:31 +0900

    feat(route): 랜딩 페이지, 메인 페이지 라우트 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,18 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'Index',
+    component: () => import('@/views/IndexPage.vue'),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/',
+    name: 'Main',
+    component: () => import('@/views/MainPage.vue'),
   },
 ]
 
---
+++ src/views/MainPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main-container">
+    <h1>Main Page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss"></style>
