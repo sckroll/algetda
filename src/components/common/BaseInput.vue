33c0e8694b09c3e9059bad2f738bd1bc21a50f2a
sckroll <kimsc0714@gmail.com>
2021-11-27 17:11:00 +0900

    feat: 입력 창 컴포넌트 구현

---
+++ src/components/common/BaseInput.vue
@@ -0,0 +1,65 @@
+<template>
+  <input
+    :type="type"
+    :placeholder="placeholder"
+    :disabled="disabled"
+    :class="{ disabled }"
+    :value="value"
+    @input="handleInput"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    value: {
+      type: String,
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+    placeholder: {
+      type: String,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    handleInput({ target }: InputEvent) {
+      this.$emit('input', (target as HTMLInputElement).value)
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+input {
+  outline: none;
+  padding: 4px 8px;
+  border: 3px solid $color-grey-1;
+  transition: all 0.15s ease;
+
+  &:hover {
+    border: 3px solid $color-primary;
+    transition: all 0.15s ease;
+  }
+  &:active,
+  &:focus {
+    border: 3px solid $color-secondary;
+    transition: all 0.15s ease;
+  }
+  &::placeholder {
+    color: $color-grey-2;
+  }
+  &.disabled {
+    border: 3px solid $color-grey-2;
+    color: $color-grey-2;
+    background-color: $color-grey-4;
+  }
+}
+</style>
