ab4a2172369a5d73c8408b579498d73532f9fd07
sckroll <kimsc0714@gmail.com>
2021-11-28 00:10:29 +0900

    feat(style): 토글 스위치 테두리 두께 및 노브 크기 조정

--- src/components/common/BaseToggleSwitch.vue
+++ src/components/common/BaseToggleSwitch.vue
@@ -36,9 +36,9 @@ export default Vue.extend({
   cursor: pointer;
   width: 48px;
   height: 24px;
-  padding: 2px;
+  padding: 3px;
   box-sizing: border-box;
-  border: 2px solid $color-grey-1;
+  border: 3px solid $color-grey-1;
   border-radius: 24px;
   display: flex;
   align-items: center;
@@ -46,20 +46,20 @@ export default Vue.extend({
   transition: all 0.15s ease;
 
   &.toggled {
-    border: 2px solid $color-secondary;
+    border: 3px solid $color-secondary;
     background-color: $color-primary;
     transition: all 0.15s ease;
   }
   &.disabled {
     cursor: default;
-    border: 2px solid $color-grey-3;
+    border: 3px solid $color-grey-3;
     background-color: $color-grey-4;
   }
 }
 .knob {
-  width: 16px;
-  height: 16px;
-  border-radius: 8px;
+  width: 12px;
+  height: 12px;
+  border-radius: 7px;
   background-color: $color-secondary;
   transition: all 0.15s ease;
 
