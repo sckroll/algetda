5601fbfb45beb8db90e47b3dd2c0a8ead5a2a0b2
sckroll <kimsc0714@gmail.com>
2021-11-29 10:46:44 +0900

    feat(style): 툴팁에 트랜지션 및 블러 효과 추가

--- src/components/common/BaseTooltip.vue
+++ src/components/common/BaseTooltip.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="tooltip">
-    <div
-      v-if="verticalPos === 'up'"
-      class="arrow-container"
-      :class="{ [horizontalPos]: horizontalPos }"
-    >
-      <div class="arrow up"></div>
+  <transition name="fade">
+    <div v-if="value" class="tooltip">
+      <div
+        v-if="verticalPos === 'up'"
+        class="arrow-container"
+        :class="{ [horizontalPos]: horizontalPos }"
+      >
+        <div class="arrow up"></div>
+      </div>
+      <div class="content"><slot></slot></div>
+      <div
+        v-if="verticalPos === 'down'"
+        class="arrow-container"
+        :class="{ [horizontalPos]: horizontalPos }"
+      >
+        <div class="arrow down"></div>
+      </div>
     </div>
-    <div class="content"><slot></slot></div>
-    <div
-      v-if="verticalPos === 'down'"
-      class="arrow-container"
-      :class="{ [horizontalPos]: horizontalPos }"
-    >
-      <div class="arrow down"></div>
-    </div>
-  </div>
+  </transition>
 </template>
 
 <script lang="ts">
@@ -23,6 +25,10 @@ import Vue from 'vue'
 
 export default Vue.extend({
   props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
     arrow: {
       type: String,
       default: 'down center',
@@ -52,6 +58,8 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
 .tooltip {
+  z-index: 5;
+  position: absolute;
   display: flex;
   flex-direction: column;
 }
@@ -60,6 +68,7 @@ export default Vue.extend({
   box-sizing: border-box;
   font-size: 0.9em;
   background-color: rgba($color-grey-4, 0.7);
+  backdrop-filter: blur(6px);
 }
 .arrow-container {
   display: flex;
@@ -76,16 +85,16 @@ export default Vue.extend({
   }
 }
 .arrow {
-  width: 0;
-  height: 0;
-  border-left: 16px solid transparent;
-  border-right: 16px solid transparent;
+  width: 32px;
+  height: 32px;
+  background-color: rgba($color-grey-4, 0.7);
+  backdrop-filter: blur(6px);
 
   &.up {
-    border-bottom: 32px solid rgba($color-grey-4, 0.7);
+    clip-path: polygon(0% 100%, 50% 0%, 100% 100%);
   }
   &.down {
-    border-top: 32px solid rgba($color-grey-4, 0.7);
+    clip-path: polygon(0% 0%, 50% 100%, 100% 0%);
   }
 }
 </style>
