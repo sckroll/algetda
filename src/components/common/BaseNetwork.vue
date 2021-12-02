0c0197934fe0fdf81156bab422b493fa49c749a6
sckroll <kimsc0714@gmail.com>
2021-12-02 23:09:38 +0900

    feat: 네트워크 그래프 컴포넌트 구현

---
+++ src/components/common/BaseNetwork.vue
@@ -0,0 +1,65 @@
+<template>
+  <D3Network
+    :net-nodes="nodes"
+    :net-links="links"
+    :options="options"
+    class="network-container"
+  ></D3Network>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import D3Network from 'vue-d3-network'
+
+interface NodeObject {
+  id: number
+  name?: string
+  _color?: string
+  _cssClass?: string
+  _labelClass?: string
+  svgSym?: SVGElement
+  _size?: number
+  _width?: number
+  _height?: number
+  _svgAttrs?: any
+}
+interface LinkObject {
+  id?: number
+  name?: string
+  tid: number
+  sid: number
+  _color?: string
+  _svgAttrs?: any
+}
+
+export default Vue.extend({
+  components: {
+    D3Network,
+  },
+  props: {
+    nodes: {
+      type: Array as PropType<NodeObject[]>,
+      required: true,
+    },
+    links: {
+      type: Array as PropType<LinkObject[]>,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      options: {
+        nodeSize: 32,
+        linkWidth: 4,
+      },
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.network-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
