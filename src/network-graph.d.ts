4cf59ce8b7c797376d38f67cf8b671c3b6fb4a23
sckroll <kimsc0714@gmail.com>
2021-12-05 01:03:12 +0900

    chore: 그래프 노드 및 간선 타입을 생성 전과 후로 분리

--- src/network-graph.d.ts
+++ src/network-graph.d.ts
@@ -1,4 +1,4 @@
-export interface NodeObject {
+export interface NewNode {
   id: number
   name: number | string
   _color?: string
@@ -10,10 +10,33 @@ export interface NodeObject {
   _height?: number
 }
 
-export interface LinkObject {
+export interface NewLink {
   id?: number
   name?: number | string
+  index?: number
   sid: number
   tid: number
   _color?: string
+}
+
+export interface NodeObject {
+  id: number
+  name: number | string
+  index: number
+  pinned: boolean
+  x: number
+  y: number
+  fx: number
+  fy: number
+  vx: number
+  vy: number
+}
+
+export interface LinkObject {
+  id: string
+  index: number
+  sid: number
+  tid: number
+  source: NodeObject
+  target: NodeObject
 }
